d91bac1b8d66bd0d5a07d26de009bd155f9dc7b9
beezies <briannaxrogers@yahoo.com>
2025-06-17 21:20:21 -0700

    comments

--- a1/myvlclient.py
+++ a1/myvlclient.py
@@ -9,14 +9,17 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 # address is represented by (host, port) tuple
 clientSocket.connect((serverName, serverPort))
 
+# take user input and send to server
 sentence = input('Input message length, followed by your lowercase sentence:\n')
 sentlen = len(sentence)
-
 clientSocket.send(sentence.encode())
+
+# receive the 64 byte server response
 modifiedSentence = clientSocket.recv(bufsize)
 reclen = bufsize
 print("From Server: ", modifiedSentence.decode())
 
+# if the sent message was longer than the 64-byte bufsuze, continue accepting until the full modified message is received
 while (reclen < sentlen):
     modifiedSentence = clientSocket.recv(bufsize)
     print("From Server: ", modifiedSentence.decode())
--- a1/myvlserver.py
+++ a1/myvlserver.py
@@ -17,14 +17,15 @@ print(f"Connection from {addr[0]}")
 # read first 64 bytes & extract message length
 message = cnSocket.recv(bufsize).decode()
 msglen = int(message[0:nbytesforlen])
-sentence = message[nbytesforlen:len(message)]
 print(f"Message length: {msglen}")
 
-while True:
+# the sentence = the message minus the length specifier 
+sentence = message[nbytesforlen:len(message)]
 
+while True:
+    # capitalize and send the 64 bytes received
     capSentence = sentence.upper()
     print(f"Processed: {sentence}")
-
     cnSocket.send(capSentence.encode())
     sentlen = len(sentence)
     print(f"Message length sent: {sentlen}")
@@ -34,6 +35,8 @@ while True:
         break
 
     sentence = cnSocket.recv(bufsize).decode()
+
+    # msglen = length of message remaining
     msglen -= bufsize 
 
 cnSocket.close()

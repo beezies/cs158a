4c6d1bca2e4d18345a87b44cd1aff92b745db285
beezies <briannaxrogers@yahoo.com>
2025-06-29 00:15:33 -0700

    completed serevr + client

---
+++ a2/mychatclient.py
@@ -0,0 +1,37 @@
+from socket import *
+import threading
+
+serverName = 'localhost'
+serverPort = 12000
+bufsize = 1024
+
+def send_messages(socket):
+    while True:
+        sendmessage = input("")
+        socket.send(sendmessage.encode())
+        if (sendmessage == "exit"):
+            socket.close()
+            break
+
+def receive_messages(socket):
+    while True:
+        try:
+            servermessage = socket.recv(bufsize).decode()
+            print(servermessage)
+        except: 
+            break
+
+# AF_INET to specify IPv4, SOCK_STREAM to use TCP
+clientSocket = socket(AF_INET, SOCK_STREAM)
+# address is represented by (host, port) tuple
+clientSocket.connect((serverName, serverPort))
+print("Connected to chat server. Type 'exit' to leave.")
+
+# to enable simultaneous message sending/receiving, use a thread for each
+send_thread = threading.Thread(target=send_messages, args=(clientSocket,))
+receive_thread = threading.Thread(target=receive_messages, args=(clientSocket,))
+send_thread.start()
+receive_thread.start()
+
+
+
---
+++ a2/mychatserver.py
@@ -0,0 +1,46 @@
+from socket import *
+import threading
+serverPort = 12000
+bufsize = 1024
+
+clients = []
+clients_lock = threading.Lock()
+
+def client_listen(cnSocket, addr):
+  print(f"New connection from ({addr[0]} : {addr[1]})")
+  # use the lock to append a new client to the list
+  with clients_lock:
+    clients.append(cnSocket)
+  # keep receiving messages from the client until they send "exit", broadcast the messages
+  while True:
+    message = cnSocket.recv(bufsize).decode()
+    if (message == "exit"):
+      break
+    full_message = str(addr[1]) + ": " + message
+    print(full_message)
+    send_message(cnSocket, addr, full_message)
+  # lock the clients list and remove the client after "exit"
+  with clients_lock:
+    clients.remove(cnSocket)
+    cnSocket.close()
+    print(f"Connection closed with {addr}")
+
+def send_message(sender, addr, message):
+  # use the lock to read through the client list & send the message to every other client
+  with clients_lock:
+    for client in clients:
+      if sender != client:
+        client.send(message.encode())
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('localhost', serverPort))
+serverSocket.listen(1)
+print(f"Server listening on {serverSocket.getsockname()[0]} : {serverPort}")
+
+while True:
+  # accept client connections
+  cnSocket, addr = serverSocket.accept()
+  # spawn a thread for each
+  client_thread = threading.Thread(target=client_listen, args=(cnSocket, addr))
+  client_thread.start()
+
